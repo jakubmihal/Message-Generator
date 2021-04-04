7ee1782059ec5c97a1aee4d8faef44c3275700c5
Jakub Mihal <jakubmihal95@gmail.com>
2021-04-04 18:15:10 +0200

    add object with message components

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 function ranNumGen(num) {
     return Math.floor(Math.random() * num);
 }
+
+// object that stores arrays with message components
+const storeMessageComponents = {
+    days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    temperature: [12, 5, 5, 7, 13, 13, 11],
+    cloudConditions: ['sunny', 'mostly sunny', 'partly sunny', 'mostly cloudy', 'cloudy']
+}
+
