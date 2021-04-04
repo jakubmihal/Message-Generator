4c38f826a0c793f52a38dc1c46221dfa67e085df
Jakub Mihal <jakubmihal95@gmail.com>
2021-04-04 18:57:54 +0200

    last update

--- script.js
+++ script.js
@@ -6,10 +6,38 @@ function ranNumGen(num) {
 // object that stores arrays with message components
 const storeMessageComponents = {
     days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    temperature: [12, 5, 5, 7, 13, 13, 11],
+    temperature: ['12', '5', '5', '7', '13', '13', '11'],
     cloudConditions: ['sunny', 'mostly sunny', 'partly sunny', 'mostly cloudy', 'cloudy']
 }
 
 // array that stores randomly choosed components
 let storeComp = []
 
+// loop iterates through object considering it's arrays length
+for(let i in storeMessageComponents) {
+    let index = ranNumGen(storeMessageComponents[i].length)
+
+
+//switch through object's properties and push them to storedComp array
+switch(i) {
+    case 'days':
+        storeComp.push(`On ${storeMessageComponents[i][index]}`)
+        break
+    case 'temperature':
+        storeComp.push(`is going to be ${storeMessageComponents[i][index]} degrees Celsius`)
+        break
+    case 'cloudConditions':
+        storeComp.push(`and cloud condition will be ${storeMessageComponents[i][index]}.`)
+        break
+    default:
+        storeComp.push(`Add more information.`)
+    }
+}
+
+// Format array into a sentence
+function format(array) {
+    const formated = array.join(' ')
+    console.log(formated)
+}
+
+format(storeComp)
